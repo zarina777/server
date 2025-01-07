a2c4cb44c4d78ad04d623129941a61d077245c1a
zarina777 <uktamovazarinabaratovna@gmail.com>
2025-01-08 02:37:07 +0500

    ASDf

--- routes/products.router.js
+++ routes/products.router.js
@@ -82,61 +82,54 @@ router.put("/:id", async (req, res) => {
     });
   }
 });
+// POST route to add a like
 router.post("/:id/likeds", async (req, res) => {
   try {
     const { id } = req.params;
-    let { liked } = req.body;
-    const productLikeds = await Products.findOne(id);
-    let product;
-    if (productLikeds) {
-      product = await Products.findByIdAndUpdate(
-        id,
-        {
-          likeds: [liked, ...productLikeds.likeds],
-        },
-        {
-          new: true,
-        }
-      );
-    } else {
-      product = await Products.findByIdAndUpdate(
-        id,
-        {
-          likeds: [liked],
-        },
-        {
-          new: true,
-        }
-      );
+    const { liked } = req.body;
+
+    const product = await Products.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
     }
-    res.json({ message: "Liked is successfully added!", product });
+
+    // If the user has not already liked the product, add the like
+    if (!product.likeds.includes(liked)) {
+      product.likeds.unshift(liked); // Adds to the beginning of the array
+      await product.save();
+      return res.status(200).json({ message: "Liked successfully added!", product });
+    } else {
+      return res.status(400).json({ message: "Product already liked by this user" });
+    }
   } catch (err) {
-    res.json({
-      message: err.message,
-    });
-  }
-});
-router.delete("/:id/likeds", async (req, res) => {
-  try {
-    const { id } = req.params;
-    let { liked } = req.body;
-    const productLikeds = await Products.findOne(id);
-    const likedsFilter = productLikeds.likeds.filter((el) => el == liked);
-    const product = await Products.findByIdAndUpdate(
-      id,
-      {
-        likeds: likedsFilter,
-      },
-      {
-        new: true,
-      }
-    );
-    res.json({ message: "Liked is successfully deleted!", product });
-  } catch (err) {
-    res.json({
-      message: err.message,
-    });
+    res.status(500).json({ message: err.message });
   }
 });
 
+// DELETE route to remove a like
+router.delete("/:id/likeds", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { liked } = req.body;
+
+    const product = await Products.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    // Filter out the user ID from the likeds array
+    const updatedLikeds = product.likeds.filter((el) => el != liked);
+
+    // If the liked array has changed, update it
+    if (updatedLikeds.length !== product.likeds.length) {
+      product.likeds = updatedLikeds;
+      await product.save();
+      return res.status(200).json({ message: "Liked successfully deleted!", product });
+    } else {
+      return res.status(400).json({ message: "User has not liked this product" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 module.exports = router;
